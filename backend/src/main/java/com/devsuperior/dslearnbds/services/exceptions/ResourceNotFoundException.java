dddaa1912a6800387a846194bcca8172c6ba8044
Rafael Zabotto <rafaelm.zabotto@gmail.com>
2022-06-03 15:50:42 -0300

    Exceptions, Validations and Security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearnbds.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 5573572785382419086L;
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
